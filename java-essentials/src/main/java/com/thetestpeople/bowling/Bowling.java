863fbe37d108e0a7e0a7bc41e8650463a223e4e1
James Salt <saltpy@gmail.com>
2014-03-18 14:33:53 +0000

    Refactor: make it nicer

--- java-essentials/src/main/java/com/thetestpeople/bowling/Bowling.java
+++ java-essentials/src/main/java/com/thetestpeople/bowling/Bowling.java
@@ -3,21 +3,31 @@ package com.thetestpeople.bowling;
 public class Bowling {
 	private int[] scores = new int[21];
 	private int nextBall = 0;
-	
+	private int nextBallToScore = 0;
+	private int total;
+
 	public int score() {
-		int total = 0;
-		int next = 0;
+		total = 0;
 		for (int frame = 0; frame < 10; frame++) {
-			if (scores[next] + scores[next + 1] == 10) {
-				total += 10 + scores[next + 2];
-				next += 2;
-			} else {
-				total += scores[next];
-				total += scores[next + 1];
-				next += 2;
-			}
+			doSpares();
+			doFaceValues();
+			advance();
 		}
 		return total;
+	}
+
+	private void advance() {
+		nextBallToScore += 2;
+	}
+
+	private void doFaceValues() {
+		total += scores[nextBallToScore] + scores[nextBallToScore + 1];
+	}
+
+	private void doSpares() {
+		if ((scores[nextBallToScore] + scores[nextBallToScore + 1]) == 10) {
+			total += scores[nextBallToScore + 2];
+		}
 	}
 
 	public void roll(int pins) {
