169512fe1763620379172a2e1bbd6f36221e2913
James Salt <saltpy@gmail.com>
2014-03-19 15:32:25 +0000

    Added: Before and After methods added

--- java-essentials/src/test/java/com/thetestpeople/add/wd/TestScratch.java
+++ java-essentials/src/test/java/com/thetestpeople/add/wd/TestScratch.java
@@ -1,14 +1,27 @@
 package com.thetestpeople.add.wd;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestScratch {
-	@Test
-	public void test() throws InterruptedException {
-		WebDriver webDriver = new ChromeDriver();
-		webDriver.get("http://www.google.com/");
-		webDriver.wait(500);
+	private static WebDriver webDriver;
+	
+	@Before
+	public void setUp() {
+		webDriver = new ChromeDriver();
 	}
+	
+	@After
+	public void tearDown() {
+		webDriver.quit();
+	}
+	
+	@Test
+	public void testCanGetGoogle() throws InterruptedException {
+		webDriver.get("http://www.google.com/");
+	}
+	
 }
